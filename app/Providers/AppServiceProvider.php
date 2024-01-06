968bb35df0568221740cd6a4af12868389324b1c
Kiet <kietanh75@gmail.com>
2024-01-06 17:26:39 +0700

    fix production env issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (env('APP_ENV') === 'production') {
-            URL::forceSchema('https');
+            URL::forceScheme('https');
         }
     }
 }
